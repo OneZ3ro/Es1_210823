18aa67b756395bef2dd4b31dbbf28cd10510ef9c
Angelo Moreno <emilioangelo665@gmail.com>
2023-08-21 16:13:26 +0200

    es class Pet (without function sameowner) done

--- script.js
+++ script.js
@@ -24,3 +24,49 @@ const userK = new User("Tommaso", "Bianchi", 30, "Modena");
 const userJ = new User("Pasquale", "Di Nozzo", 20, "Napoli");
 
 console.log(User.diffAge(userX, userZ));
+
+//____________________________________________________________________________________________________________
+
+const animalArr = [];
+const lista = document.getElementById("lista");
+class Pet {
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+  }
+
+  static isSameOwner(animalX, animalY) {
+    // console.log("Funziona");
+    if (animalX.ownerName === animalY.ownerName) {
+      return (
+        animalX.petName + " e " + animalY.petName + " hanno lo stesso padrone"
+      );
+    }
+  }
+}
+
+const createAnimal = function (event) {
+  event.preventDefault();
+  const input = document.querySelectorAll("input");
+  const arr = [];
+
+  for (let i = 0; i < input.length; i++) {
+    arr.push(input[i].value);
+  }
+
+  const animal = new Pet(arr[0], arr[1], arr[2], arr[3]);
+  animalArr.push(animal);
+
+  const li = document.createElement("li");
+  li.innerText = `Nome animale: ${animal.petName};\n Nome padrone: ${animal.ownerName};\n Specie ${animal.petName}: ${animal.species} ${animal.breed}`;
+  lista.appendChild(li);
+
+  console.log(arr);
+  console.log(animal);
+  console.log(animalArr);
+  //   if (animalArr >= 2) {
+  //     console.log(Pet.isSameOwner(animalArr[0], animalArr[1]));
+  //   }
+};
